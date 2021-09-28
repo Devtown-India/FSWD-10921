f7e360836c4d508069ee224805f2654c3d841af4
Kartik18g <gupta.kartik18kg@gmail.com>
2021-09-28 19:38:14 +0530

    operators

--- app.js
+++ app.js
@@ -1,4 +1,59 @@
-var age = null
+// Operators
 
-// console.log(Name)
-console.log(typeof age)
+
+var a = 2
+var b = 10
+
+// Arithmetic operators
+
+console.log('a + b =', a + b)
+console.log('b - a =', b - a)
+console.log('b / a =', b / a)
+console.log('b * a =', b * a)
+console.log('b ** a =', b ** a)
+console.log('b % a =', b % a)
+
+
+// Assignment Operators
+console.log('b ', b)
+b += 10
+console.log('b+=10 ', b)
+b++
+console.log('b++ ', b)
+b -= 10
+console.log('b-=10 ', b)
+b--
+console.log('b-- ', b)
+
+var c = 10
+
+console.warn(c++)
+console.log(c)
+// console.warn(++c)
+
+// Comparison operators
+
+var d = 10
+var e = 2
+var f = '10'
+
+console.log(d > e)
+console.log(d < e)
+console.log(d >= e)
+console.log(d <= e)
+console.log(d == e)
+
+console.warn(d == f)
+console.warn(d === f)
+
+// Logical operators
+
+console.log('true && true ', true && true)
+console.log('true && false', true && false)
+console.log('false && true', false && true)
+console.log('false && false', false && false)
+
+console.log('true || true ', true || true)
+console.log('true || false', true || false)
+console.log('false || true', false || true)
+console.log('false || false', false || false)
