eb9bdd9c71770b4937999110c16b123d268ad6be
Kartik18g <gupta.kartik18kg@gmail.com>
2021-09-28 19:09:35 +0530

    null vs undefined

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-// string
-console.log(typeof "Hello World")
-console.log(typeof "d")
+var age = null
 
-// Numbers
-console.log(typeof 1)
-console.log(typeof 1.2)
-console.log(typeof 112341234932904902134902190349090)
-console.log(typeof -1)
-console.log(typeof -1.2)
-
-// boolean
-console.log(typeof true)
-console.log(typeof false)
+// console.log(Name)
+console.log(typeof age)
