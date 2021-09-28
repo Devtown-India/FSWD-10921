5a95067959f7aae6899502f3a59d5715c5570498
Kartik18g <gupta.kartik18kg@gmail.com>
2021-09-28 18:55:56 +0530

    primitive data-types

---
+++ app.js
@@ -0,0 +1,14 @@
+// string
+console.log(typeof "Hello World")
+console.log(typeof "d")
+
+// Numbers
+console.log(typeof 1)
+console.log(typeof 1.2)
+console.log(typeof 112341234932904902134902190349090)
+console.log(typeof -1)
+console.log(typeof -1.2)
+
+// boolean
+console.log(typeof true)
+console.log(typeof false)
