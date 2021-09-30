536c263d9956f00d433003ed7b0a92ec9d4d4ab5
Kartik18g <gupta.kartik18kg@gmail.com>
2021-09-30 20:20:01 +0530

    dark mode

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
+var body = document.querySelector('body')
 var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
 
 
 const callbackfunc = (event) => {
-    // console.log(input.value)
-    const inputValue = input.value
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
+    body.classList.toggle('dark')
 }
 
 
