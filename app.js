0a0db4719e3b11e64af0243b54c0898f777819bf
Kartik18g <gupta.kartik18kg@gmail.com>
2021-09-30 19:12:38 +0530

    functions

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-var list = document.querySelector('ul')
+// function greet(name = 'John', age = 99) {
+//     // console.log('Hello')
+//     console.log(name, age)
+//     // console.log('Hey ' + name + " Your age is " + age)/
+// }
+
+// greet()
 
 
-var arr = ['Go the GYM ', 'cook oatmeal', 'Eat']
 
-arr.push('Complete the assignment')
+// function add(a = 0, b = 0) {
+//     var sum = a + b
+//     return sum
+// }
 
-for (var i = 0; i < arr.length; i++) {
-    var element = document.createElement('li')
-    var textNode = document.createTextNode(arr[i])
-    element.appendChild(textNode)
-    list.appendChild(element)
-
+var add = (a = 0, b = 0) => {
+    var sum = a + b
+    return sum
 }
+
+
+
+console.log(add(10, 20))
