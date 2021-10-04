ccbb29243c829be563d1c59e463c2843c9d76a89
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-04 16:02:34 +0530

    fc

--- app.js
+++ app.js
@@ -2,14 +2,29 @@ var button = document.querySelector('button')
 var input = document.querySelector('input')
 var list = document.querySelector('ul')
 
+var chores = []
+
+var deleteItem = (value) => {
+    const index = chores.indexOf(value)
+    chores.splice(index, 1)
+    console.log(chores)
+}
+
 
 const callbackfunc = (event) => {
     // console.log(input.value)
     const inputValue = input.value
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
+    if (chores.includes(inputValue)) {
+        console.log('already exists')
+    }
+    else {
+        chores.push(inputValue)
+        const element = document.createElement('li')
+        const textNode = document.createTextNode(inputValue)
+        element.appendChild(textNode)
+        list.appendChild(element)
+        element.addEventListener('click', (e) => { e.target.remove() })
+    }
 }
 
 
