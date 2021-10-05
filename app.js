b9718b2aa05fc93bfe6cda554b39e6154c5f6f3a
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-05 20:01:12 +0530

    Promises

--- app.js
+++ app.js
@@ -1,31 +1,6 @@
-var button = document.querySelector('button')
-var input = document.querySelector('input')
-var list = document.querySelector('ul')
-
-var chores = []
-
-var deleteItem = (value) => {
-    const index = chores.indexOf(value)
-    chores.splice(index, 1)
-    console.log(chores)
-}
+let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {
+    console.log(data)
+})
 
 
-const callbackfunc = (event) => {
-    // console.log(input.value)
-    const inputValue = input.value
-    if (chores.includes(inputValue)) {
-        console.log('already exists')
-    }
-    else {
-        chores.push(inputValue)
-        const element = document.createElement('li')
-        const textNode = document.createTextNode(inputValue)
-        element.appendChild(textNode)
-        list.appendChild(element)
-        element.addEventListener('click', (e) => { e.target.remove() })
-    }
-}
 
-
-button.addEventListener('click', callbackfunc)
