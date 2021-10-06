5abb0dcad9306a0ea3a613d7667b1b970f609e79
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-06 18:33:31 +0530

    delete todo item

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(data => {
-    console.log(data)
+// let response = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json())
+const list = document.querySelector('ul')
+
+
+
+let arr = ['One', 'Two', 'Three', 'Four']
+
+arr.forEach(chore => {
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(chore)
+    element.appendChild(textNode)
+
+    element.addEventListener('click', (e) => {
+        const userInput = window.prompt(`Are you sure that you want to delete "${chore}"`)
+        if (userInput === 'YES') e.target.remove()
+    })
+
+    list.appendChild(element)
 })
-
-
-
