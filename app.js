95fc6836a873e0245e054673e1872fde6d66e53a
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-07 19:34:51 +0530

    recipe app complete

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
-// const ul = document.querySelector('ul')
-// const but = document.querySelector('button')
-// const getText = document.querySelector('input')
-// const parent = document.querySelector('div.grid')
+const ul = document.querySelector('ul')
+const but = document.querySelector('button')
+const getText = document.querySelector('input')
+const parent = document.querySelector('div.grid')
 
-// const getData = async (item) => {
-//     console.log(item)
-//     const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
-//     console.log(url)
-//     const res = await fetch(url)
-//     const data = await res.json()
-//     const recipies = data.hits
-//     recipies.forEach(item => {
-//         console.log(item)
-//         console.log(item.recipe.label)
-//         const template = ` 
-//                         <img class="card__image" src="https://i.ibb.co/RT0bjJq/food1.png" />
-//                         <div class="card__data">
-//                             <div class="card__info">
-//                                 <h2>Nombre Comida</h2>
-//                                 <p>Descripcion de la comida, con ingredientes</p>
-//                             </div>
-//                             <h3 class="card__price">$7.50</h3>
-//                             <button class="card__add">+</button>
-//                         </div>
-//                     `
-//         const newCard = document.createElement('article')
-//         newCard.setAttribute('class', 'card')
-//         newCard.setAttribute('style', 'margin-bottom:20px')
-//         newCard.innerHTML = template
-//         parent.appendChild(newCard)
-//     })
+const getData = async (item) => {
+    console.log(item)
+    const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    console.log(url)
+    const res = await fetch(url)
+    const data = await res.json()
+    const recipies = data.hits
+    recipies.forEach(item => {
+        const { recipe } = item
+        const { label, image, source, calories } = recipe
+        const template = ` 
+                        <img class="card__image" src="${image}" />
+                        <div class="card__data">
+                            <div class="card__info">
+                                <h2>${label}</h2>
+                                <p>From ${source}</p>
+                            </div>
+                            <h3 class="card__price">${calories.toFixed(2)}</h3>
+                            <button class="card__add">+</button>
+                        </div>
+                    `
+        const newCard = document.createElement('article')
+        newCard.setAttribute('class', 'card')
+        newCard.setAttribute('style', 'margin-bottom:50px')
+        newCard.innerHTML = template
+        parent.appendChild(newCard)
+    })
 
-// }
+}
 
-// //  add event listener to the button
-// but.addEventListener('click', (e) => {
-//     getData(getText.value)
-// })
-
-const person = ({
-    name: "Prem",
-    age: 19,
-    country: "India"
+//  add event listener to the button
+but.addEventListener('click', (e) => {
+    getData(getText.value)
 })
-
-const { age, nName, country } = person
-
-console.log(nName, age, country)
-
-// console.log(`Hi ${name} you are ${age}`)
