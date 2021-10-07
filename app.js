71de0c3014bfa3c6195f5b5d1311ed80c795f2f1
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-07 18:47:47 +0530

    recipe app

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
-const button = document.querySelector('button')
-const input = document.querySelector('input')
-
+const ul = document.querySelector('ul')
+const but = document.querySelector('button')
+const getText = document.querySelector('input')
 const getData = async (item) => {
     console.log(item)
-    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
+    const url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q=${item}`
     console.log(url)
     const res = await fetch(url)
     const data = await res.json()
-    console.log()
-    data.hits.forEach(recipe => {
-        console.log(recipe)
-    })
+    console.log(data.hits)
 }
 
 //  add event listener to the button
-button.addEventListener('click', (e) => {
-    getData(input.value)
+but.addEventListener('click', (e) => {
+    getData(getText.value)
 })
