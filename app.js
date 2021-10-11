a4c9970452e40ba89c2273a949bafc570b2e7e42
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-11 18:18:42 +0530

    local storage

--- app.js
+++ app.js
@@ -1,16 +1,11 @@
-// Node.js program to demonstrate the
-// fs.writeFileSync() method
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
 
-// Import the filesystem module
-const fs = require('fs');
+const arr = [1, 2, 3, 4, 5]
 
-let data = "This is a file containing a collection"
-    + " of programming languages.\n"
-    + "1. C\n2. C++\n3. Python";
 
-fs.wite("./file/programming.txt", data, (a, b) => {
-    console.log(a, b)
-});
-console.log("File written successfully\n");
-console.log("The written has the following contents:");
-console.log(fs.readFileSync("./file/programming.txt", "utf8"));
+
+localStorage.setItem('val', JSON.stringify(arr))
+
+console.log(JSON.parse(localStorage.getItem('val')))
