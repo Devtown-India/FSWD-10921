1eb894556c6fd19132a672663c1dc38c5047cf23
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-11 16:15:32 +0530

    s

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
-const obj1 = {
-    name: "McQueen",
-    email: "cars@cars.com"
-}
-// PUT
-const obj2 = {
-    name: "Mater",
-}
+// Node.js program to demonstrate the
+// fs.writeFileSync() method
 
-const result = {
-    name: "Mater",
-}
-// PATCH
-const obj2 = {
-    name: "Mater",
-}
+// Import the filesystem module
+const fs = require('fs');
 
-const result = {
-    name: "Mater",
-    email: "cars@cars.com"
-}
+let data = "This is a file containing a collection"
+    + " of programming languages.\n"
+    + "1. C\n2. C++\n3. Python";
+
+fs.wite("./file/programming.txt", data, (a, b) => {
+    console.log(a, b)
+});
+console.log("File written successfully\n");
+console.log("The written has the following contents:");
+console.log(fs.readFileSync("./file/programming.txt", "utf8"));
