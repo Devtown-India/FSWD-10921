6e77a360545c1626055477209516b71866dcf304
Kartik18g <gupta.kartik18kg@gmail.com>
2021-10-12 20:03:23 +0530

    express server

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+// const http = require('http')
+// const fs = require('fs')
+// const path = require('path')
 
-const server = http.createServer((request, response) => {
-    // request.url
-    // console.log(request.headers)
-    const markup = fs.readFileSync(path.resolve('./index.html'))
-    response.write(markup)
-    response.end()
+// const server = http.createServer((request, response) => {
+//     // request.url
+//     const { url } = request
+//     if (url == '/login') {
+//         response.write('<h1>LOGIN</h1>')
+//         response.end()
+//     }
+//     if (url == '/signup') {
+//         response.write('<h1>SINGUP</h1>')
+//         response.end()
+//     }
+// })
+
+
+// server.listen(3000, () => {
+//     console.log(`Server listening at PORT: ${3000}`)
+// })
+
+const data = [{ name: "Anisha" }, { name: "Suman" }, { name: "Varun" }]
+
+const express = require('express')
+const app = express()
+
+app.get('/getNames', (req, res) => {
+    res.send(data)
 })
 
 
-server.listen(3000, () => {
-    console.log(`Server listening at PORT: ${3000}`)
+app.listen(3000, () => {
+    console.log("server listening at port 3000")
 })
-
