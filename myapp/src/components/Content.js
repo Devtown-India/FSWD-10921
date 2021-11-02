dd10139100b766f82a8bb0b86770bcf0cbe43d31
Kartik18g <gupta.kartik18kg@gmail.com>
2021-11-02 19:20:11 +0530

    s

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -7,19 +7,25 @@ const Content = () => {
 
     const handleSubmit = () => {
         setItems(prev => [...prev, value])
+        setValue('')
     }
 
     const handleInput = (e) => {
         setValue(e.target.value)
     }
 
+    const handleDelete = (item) => {
+        console.log(item)
+        setItems(prev => prev.filter(i => i !== item))
+    }
+
     return (
         <>
             <h2>Todo</h2>
-            <input onChange={handleInput} type="text" />
+            <input value={value} onChange={handleInput} type="text" />
             <button onClick={handleSubmit} type="submit">Add</button>
             <ul>
-                {items.map(item => <li>{item}</li>)}
+                {items.map(item => <li>{item} <button onClick={() => { handleDelete(item) }}>Delete</button> </li>)}
             </ul>
         </>
     );
