45c45ca1ada9e2a8b7e74fd3386bc0d1bfd05b03
Kartik18g <gupta.kartik18kg@gmail.com>
2021-11-08 13:20:15 +0530

    s

--- myapp/src/components/Content.js
+++ myapp/src/components/Content.js
@@ -1,34 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 
 const Content = () => {
 
-    const [items, setItems] = useState([])
-    const [value, setValue] = useState('')
+    const [counter, setCounter] = useState(0)
+    const [content, setContent] = useState([])
 
-    const handleSubmit = () => {
-        setItems(prev => [...prev, value])
-        setValue('')
+    const APICall = async () => {
+        const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+        const data = await res.json()
+        setContent(data)
     }
 
-    const handleInput = (e) => {
-        setValue(e.target.value)
-    }
+    useEffect(() => {
+        APICall()
+    }, [])
+    useEffect(() => {
+        console.log('counter changed')
+    }, [counter])
 
-    const handleDelete = (item) => {
-        console.log(item)
-        setItems(prev => prev.filter(i => i !== item))
-    }
 
     return (
-        <>
-            <h2>Todo</h2>
-            <input value={value} onChange={handleInput} type="text" />
-            <button onClick={handleSubmit} type="submit">Add</button>
+        <div>
+            <h2>{counter}</h2>
+            <button onClick={() => { setCounter(prev => prev + 1) }} >Click Me </button>
             <ul>
-                {items.map(item => <li>{item} <button onClick={() => { handleDelete(item) }}>Delete</button> </li>)}
+                {content.map(item => <li style={{ margin: "10px", border: "3px solid black" }} >{item.title}</li>)}
             </ul>
-        </>
-    );
+        </div>);
 }
 
 export default Content;
